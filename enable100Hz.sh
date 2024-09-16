856a366a0d1775ce86c133b98793825c5f7433a2
fer-hnndz <65425622+fer-hnndz@users.noreply.github.com>
2024-09-16 11:38:52 -0600

    Add script to enable 100 Hz on display

---
+++ enable100Hz.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# © Jorge F. Hernández, 2024
+
+# I made this script so I can automate the process of enabling 100Hz on my main monitor,
+# since I won't always need the 100Hz.
+
+# The values after the mode name are specific to what Xorg logs based on driver's desired resolution.
+# For more info, refer to:
+# https://wiki.archlinux.org/title/Xrandr#Adding_undetected_resolutions
+
+xrandr --newmode "1080p100hz" 229.54 1920 1968 2000 2035 1080 1083 1088 1128 +hsync +vsync
+xrandr --addmode HDMI-A-0 1080p100hz
