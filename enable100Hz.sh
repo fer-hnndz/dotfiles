79dbd133ab548bf00a30595a2383c750ee5e091f
fer-hnndz <65425622+fer-hnndz@users.noreply.github.com>
2024-09-18 10:06:02 -0600

    Fix emable 100 hz script

--- enable100Hz.sh
+++ enable100Hz.sh
@@ -8,5 +8,5 @@
 # For more info, refer to:
 # https://wiki.archlinux.org/title/Xrandr#Adding_undetected_resolutions
 
-xrandr --newmode "1080p100hz" 229.54 1920 1968 2000 2035 1080 1083 1088 1128 +hsync +vsync
-xrandr --addmode HDMI-A-0 1080p100hz
+xrandr --newmode "1920x1080_100.00" 229.54 1920 1968 2000 2035 1080 1083 1088 1128 +hsync +vsync
+xrandr --addmode HDMI-A-0 1920x1080_100.00
