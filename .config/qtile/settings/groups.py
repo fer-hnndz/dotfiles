e0f944c8c79aca238c19a014ffefc19260202930
fer-hnndz <65425622+fer-hnndz@users.noreply.github.com>
2025-01-19 18:32:29 -0600

    Delete unused configs

--- .config/qtile/settings/groups.py
+++
@@ -1,6 +0,0 @@
-from .keys import keys, mod
-from libqtile.config import Group, Key
-from libqtile.lazy import lazy
-
-# Install a NerdFont to see these icons
-groups = [Group(name) for name in ["󰘦", "", "", "", ""]]
